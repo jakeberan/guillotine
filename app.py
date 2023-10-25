a5704263f96066613da011490c44bf02d5af64bd
Jacob Beran <37278219+jakeberan@users.noreply.github.com>
2023-10-24 22:03:48 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.header('Cedarburg Guillotine')
 
 # return week slider
 week_tuple = st.slider('Select Week',
-                   value=[5,6], 
+                   value=[7,8], 
                    min_value = 1,
                    max_value = 18)
 
